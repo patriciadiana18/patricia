a87e3fb66c77dbf2359e02338374d7eacbd3cbb0
Costa D.F. Patricia-Diana <cti22a208@cst.uoradea.local>
2023-10-16 20:40:06 +0300

    Initial commit

---
+++ src/main/java/luiziana.java
@@ -0,0 +1,9 @@
+public class luiziana {
+    public static void main(String[]args){
+        System.out.println("Hello World!How are you today?");
+        System.out.println("Who cares?");
+        System.out.println("Raul.");
+        System.out.print("Great! How is Raul today? ");
+        System.out.print("Stupid.");
+    }
+}
